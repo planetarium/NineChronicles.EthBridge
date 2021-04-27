fd9abb9fe110edd8f004331fea43092973ff846e
Moreal <dev.moreal@gmail.com>
2021-04-27 22:51:22 +0900

    feat(bridge): relay burn event

--- bridge/src/index.ts
+++ bridge/src/index.ts
@@ -4,6 +4,7 @@ import Web3 from "web3";
 import { BurnEventResult } from "./interfaces/burn-event-result";
 import { INCGTransfer } from "./interfaces/ncg-transfer";
 import { Monitor } from "./monitor";
+import { NCGTransfer } from "./ncg-transfer";
 import { wNCGToken } from "./wrapped-ncg-token";
 
 config();
@@ -14,20 +15,28 @@ if (WEB_SOCKET_PROVIDER_URI === undefined) {
     process.exit(-1);
 }
 
+const GRAPHQL_API_ENDPOINT: string | undefined = process.env.GRAPHQL_API_ENDPOINT;
+if (GRAPHQL_API_ENDPOINT === undefined) {
+    console.error("Please set 'GRAPHQL_API_ENDPOINT' at .env");
+    process.exit(-1);
+}
+
+const BRIDGE_9C_ADDRESS: string | undefined = process.env.BRIDGE_9C_ADDRESS;
+if (BRIDGE_9C_ADDRESS === undefined) {
+    console.error("Please set 'BRIDGE_9C_ADDRESS' at .env");
+    process.exit(-1);
+}
+
 (async () => {
     const CONFIRMATIONS = 10;
 
-    const ncgTransfer: INCGTransfer = {
-        transfer: (address, amount) => {
-            console.log(address, amount);
-        }
-    };
-
+    const ncgTransfer: INCGTransfer = new NCGTransfer(GRAPHQL_API_ENDPOINT, BRIDGE_9C_ADDRESS);
     const web3 = new Web3(new Web3.providers.WebsocketProvider(WEB_SOCKET_PROVIDER_URI));
     const monitor = new Monitor(web3, wNCGToken, await web3.eth.getBlockNumber(), CONFIRMATIONS);
-    const unsubscribe = monitor.subscribe(eventLog => {
+    const unsubscribe = monitor.subscribe(async eventLog => {
         const burnEventResult = eventLog.returnValues as BurnEventResult;
-        ncgTransfer.transfer(burnEventResult._sender, BigInt(burnEventResult.amount));
+        const txId = await ncgTransfer.transfer(burnEventResult._sender, BigInt(burnEventResult.amount));
+        console.log("Transferred", txId);
     });
     monitor.run();
 })();
