ab37355a2a32c3969aa2fe20853d88afed3f71b2
Moreal <dev.moreal@gmail.com>
2021-05-18 18:58:14 +0900

    fix(bridge): build url correctly

--- bridge/src/headless-http-client.ts
+++ bridge/src/headless-http-client.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { URL } from "url";
 import { IHeadlessHTTPClient } from "./interfaces/headless-http-client";
 
 export class HeadlessHTTPClient implements IHeadlessHTTPClient {
-    private readonly _rootApiEndpoint: string;
+    private readonly _rootApiEndpoint: URL;
 
     constructor(rootApiEndpoint: string) {
-        this._rootApiEndpoint = rootApiEndpoint;
+        this._rootApiEndpoint = new URL(rootApiEndpoint);
     }
 
     setPrivateKey(privateKey: string): Promise<void> {
-        const url = this._rootApiEndpoint + "/set-private-key";
-        return axios.post(url, {
+        const url = new URL("/set-private-key", this._rootApiEndpoint);
+        return axios.post(url.toString(), {
             PrivateKeyString: privateKey,
         });
     }
