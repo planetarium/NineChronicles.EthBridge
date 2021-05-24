3cd1c82ddd7d8d1535d8f9bf900ca9f1d0c1fa5a
Moreal <dev.moreal@gmail.com>
2021-05-24 18:23:06 +0900

    fix(bridge): catch and handle error while monitoring

--- bridge/src/confirmation-monitor.ts
+++ bridge/src/confirmation-monitor.ts
@@ -1,4 +1,5 @@
 import { Monitor } from "./monitor";
+import { captureException } from "@sentry/node";
 
 function delay(ms: number): Promise<void> {
     return new Promise(resolve => {
@@ -23,20 +24,25 @@ export abstract class ConfirmationMonitor<TEventData> extends Monitor<TEventData
 
     async * loop(): AsyncIterableIterator<TEventData> {
         while(true) {
-            const beforeLatestBlockNumber = this.latestBlockNumber;
-            const networkLatestBlockNumber = await this.getTipIndex();
-            const confrimedLatestBlockNumber = Math.max(networkLatestBlockNumber - this._confirmations, beforeLatestBlockNumber);
+            try {
+                const beforeLatestBlockNumber = this.latestBlockNumber;
+                const networkLatestBlockNumber = await this.getTipIndex();
+                const confrimedLatestBlockNumber = Math.max(networkLatestBlockNumber - this._confirmations, beforeLatestBlockNumber);
 
-            if (beforeLatestBlockNumber < confrimedLatestBlockNumber) {
-                this.debug(`Trying to look up from ${beforeLatestBlockNumber} to ${confrimedLatestBlockNumber}`);
-                const eventLogs = await this.getEvents(beforeLatestBlockNumber, confrimedLatestBlockNumber);
-                for (const eventLog of eventLogs) {
-                    yield eventLog;
+                if (beforeLatestBlockNumber < confrimedLatestBlockNumber) {
+                    this.debug(`Trying to look up from ${beforeLatestBlockNumber} to ${confrimedLatestBlockNumber}`);
+                    const eventLogs = await this.getEvents(beforeLatestBlockNumber, confrimedLatestBlockNumber);
+                    for (const eventLog of eventLogs) {
+                        yield eventLog;
+                    }
+
+                    this.latestBlockNumber = confrimedLatestBlockNumber + 1;
+                } else {
+                    this.debug("Skipped...");
                 }
-
-                this.latestBlockNumber = confrimedLatestBlockNumber + 1;
-            } else {
-                this.debug("Skipped...");
+            } catch (error) {
+                this.error("Ignore and continue loop without breaking though unexpected error occurred:", error);
+                captureException(error);
             }
 
             await delay(this._delayMilliseconds);
@@ -45,6 +51,10 @@ export abstract class ConfirmationMonitor<TEventData> extends Monitor<TEventData
 
     private debug(message?: any, ...optionalParams: any[]): void {
         console.debug(`[${this.constructor.name}]`, message, ...optionalParams);
+    }
+
+    private error(message?: any, ...optionalParams: any[]): void {
+        console.error(`[${this.constructor.name}]`, message, ...optionalParams);
     }
 
     protected abstract getTipIndex(): Promise<number>;
