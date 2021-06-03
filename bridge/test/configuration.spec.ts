4049fcaf0b2d9a7d24d95b2eb6f510eaa1b62756
Moreal <dev.moreal@gmail.com>
2021-06-03 10:09:31 +0900

    test(bridge): use jest instead mocha

--- bridge/test/configuration.spec.ts
+++ bridge/test/configuration.spec.ts
@@ -1,57 +1,56 @@
-import { assert } from "chai";
 import { Configuration } from "../src/configuration";
 
 const ENV_NAME = "NAME";
 
 describe("Configuration", () => {
     describe("get", () => {
-        context("with valid value", () => {
+        describe("with valid value", () => {
             it("should return string value", () => {
                 process.env[ENV_NAME] = "VALUE";
-                assert.equal(Configuration.get(ENV_NAME), "VALUE");
-                assert.equal(Configuration.get(ENV_NAME, false), "VALUE");
-                assert.equal(Configuration.get("UNDEFINED", false), undefined);
+                expect(Configuration.get(ENV_NAME)).toEqual("VALUE");
+                expect(Configuration.get(ENV_NAME, false)).toEqual("VALUE");
+                expect(Configuration.get("UNDEFINED", false)).toEqual(undefined);
             });
 
             it("should return integer value", () => {
                 const expected = 100;
                 process.env[ENV_NAME] = expected.toString();
-                assert.equal(Configuration.get(ENV_NAME, true, "integer"), expected);
-                assert.equal(Configuration.get(ENV_NAME, false, "integer"), expected);
-                assert.equal(Configuration.get("UNDEFINED", false, "integer"), undefined);
+                expect(Configuration.get(ENV_NAME, true, "integer")).toEqual(expected);
+                expect(Configuration.get(ENV_NAME, false, "integer")).toEqual(expected);
+                expect(Configuration.get("UNDEFINED", false, "integer")).toEqual(undefined);
             });
 
             it("should return 'TRUE' as boolean value", () => {
                 process.env[ENV_NAME] = "TRUE";
-                assert.equal(Configuration.get(ENV_NAME, true, "boolean"), true);
-                assert.equal(Configuration.get(ENV_NAME, false, "boolean"), true);
-                assert.equal(Configuration.get("UNDEFINED", false, "boolean"), false);
+                expect(Configuration.get(ENV_NAME, true, "boolean")).toEqual(true);
+                expect(Configuration.get(ENV_NAME, false, "boolean")).toEqual(true);
+                expect(Configuration.get("UNDEFINED", false, "boolean")).toEqual(false);
             });
 
             it("should return 'FALSE' as boolean value", () => {
                 process.env[ENV_NAME] = "FALSE";
-                assert.equal(Configuration.get(ENV_NAME, true, "boolean"), false);
-                assert.equal(Configuration.get(ENV_NAME, false, "boolean"), false);
-                assert.equal(Configuration.get("UNDEFINED", false, "boolean"), false);
+                expect(Configuration.get(ENV_NAME, true, "boolean")).toEqual(false);
+                expect(Configuration.get(ENV_NAME, false, "boolean")).toEqual(false);
+                expect(Configuration.get("UNDEFINED", false, "boolean")).toEqual(false);
             });
         });
 
         for (const testcase of [".1", "FALSE", "", "*"]) {
-            context(`with incorrect number '${testcase}'`, () => {
+            describe(`with incorrect number '${testcase}'`, () => {
                 it("should throw error", () => {
                     process.env[ENV_NAME] = testcase;
-                    assert.throw(() => Configuration.get(ENV_NAME, true, "integer"));
-                    assert.throw(() => Configuration.get(ENV_NAME, false, "integer"));
+                    expect(() => Configuration.get(ENV_NAME, true, "integer")).toThrowError();
+                    expect(() => Configuration.get(ENV_NAME, false, "integer")).toThrowError();
                 });
             });
         }
 
         for (const testcase of [".", "", "NOT", "TRON"]) {
-            context(`with incorrect boolean '${testcase}'`, () => {
+            describe(`with incorrect boolean '${testcase}'`, () => {
                 it("should throw error", () => {
                     process.env[ENV_NAME] = testcase;
-                    assert.throw(() => Configuration.get(ENV_NAME, true, "boolean"));
-                    assert.throw(() => Configuration.get(ENV_NAME, false, "boolean"));
+                    expect(() => Configuration.get(ENV_NAME, true, "boolean")).toThrowError();
+                    expect(() => Configuration.get(ENV_NAME, false, "boolean")).toThrowError();
                 });
             });
         }
