f2ad256a0a7d767be399302eac5d7957ed83f78d
Moreal <dev.moreal@gmail.com>
2021-06-03 10:37:31 +0900

    fix(bridge): correct url combination logic

--- bridge/src/messages/wrapping-event.ts
+++ bridge/src/messages/wrapping-event.ts
@@ -2,6 +2,7 @@ import { ChatPostMessageArguments } from "@slack/web-api";
 import { Message } from ".";
 import { TxId } from "../types/txid";
 import { URL } from "url";
+import { join, resolve } from "path";
 
 export abstract class WrappingEvent implements Message {
     private readonly _explorerUrl: string;
@@ -22,7 +23,11 @@ export abstract class WrappingEvent implements Message {
         return this.combineUrl(this._etherscanUrl, `/tx/${transactionHash}`);
     }
 
-    private combineUrl(url: string, addition: string): string {
-        return new URL(addition, url).toString();
+    private combineUrl(base: string, addition: string): string {
+        const [path, query] = addition.split("?");
+        const url = new URL(base);
+        url.pathname = join(url.pathname, path);
+        url.search = `?${query || ""}`;
+        return url.toString();
     }
 }
