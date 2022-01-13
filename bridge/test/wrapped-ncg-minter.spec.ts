db649eaafeecefca45a2d79d1f4d7b059cf652a2
moreal <dev.moreal@gmail.com>
2022-01-13 22:08:15 +0900

    feat(bridge): remove `gasPrice` parameter unused in EIP-1559

--- bridge/test/wrapped-ncg-minter.spec.ts
+++ bridge/test/wrapped-ncg-minter.spec.ts
@@ -63,7 +63,6 @@ describe(WrappedNCGMinter.name, () => {
             expect(mockContract.methods.mint).toHaveBeenCalledWith("0x1111111111111111111111111111111111111111", 10)
             expect(mockContractMethodReturn.send).toHaveBeenCalledWith({
                 from: mockMinterAddress,
-                gasPrice: "150",
                 maxPriorityFeePerGas: "1000000000",
                 type: "0x2",
             })
