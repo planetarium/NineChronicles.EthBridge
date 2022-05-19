766b822c73129a80e186422c2c9567e316a140ca
moreal <dev.moreal@gmail.com>
2022-05-19 11:30:11 +0900

    fix(bridge): limit `channel` field in type-level

--- bridge/src/messages/index.ts
+++ bridge/src/messages/index.ts
@@ -1,9 +1,10 @@
 import { ChatPostMessageArguments } from "@slack/web-api";
 import { URL } from "url";
 import { join } from "path";
+import { ForceOmit } from "../types/force-omit";
 
 export interface Message {
-    render(): Partial<ChatPostMessageArguments>;
+    render(): ForceOmit<Partial<ChatPostMessageArguments>, "channel">;
 }
 
 export function combineUrl(base: string, addition: string): string {
