193eadc528a9e3159147eb3ab2ea2c3fff75029e
moreal <dev.moreal@gmail.com>
2022-06-23 13:50:21 +0900

    refactor(bridge-www): provide a way to set default value

--- bridge-www/src/components/TextInput.tsx
+++ bridge-www/src/components/TextInput.tsx
@@ -1,9 +1,10 @@
 export interface TextInputProps {
     onChange: (value: string) => void,
+    value?: string,
 }
 
-export const TextInput: React.FC<TextInputProps> = ({ onChange }) => {
+export const TextInput: React.FC<TextInputProps> = ({ onChange, ...others }) => {
     return (
-        <input type="text" onChange={event => { onChange(event.target.value) }}/>
+        <input type="text" onChange={event => { onChange(event.target.value) }} {...others} />
     )
 }
