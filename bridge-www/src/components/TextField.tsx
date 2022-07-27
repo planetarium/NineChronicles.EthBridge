a85d6e07cf1af03b88603a06d8c1e1a0406f5dd1
moreal <dev.moreal@gmail.com>
2022-07-27 11:33:56 +0900

    feat(bridge-www): make text field readonly-able

--- bridge-www/src/components/TextField.tsx
+++ bridge-www/src/components/TextField.tsx
@@ -1,15 +1,16 @@
 import { Input } from "@nextui-org/react";
 
 export interface TextInputProps {
-    onChange: (value: string) => void,
+    onChange?: (value: string) => void,
     value?: string,
     label: string,
+    readOnly?: boolean,
 }
 
 export const TextField: React.FC<TextInputProps> = ({ onChange, label, ...others }) => {
     return (
         <div>
-            <Input labelLeft={label} onChange={event => { onChange(event.target.value) }} {...others} />
+            <Input labelLeft={label} onChange={event => { if (onChange !== undefined) onChange(event.target.value) }} {...others} />
         </div>
     )
 }
