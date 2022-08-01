a374ba0c778be4f59e20aab156a20921078bceff
moreal <dev.moreal@gmail.com>
2022-08-01 14:57:18 +0900

    Revert "feat(bridge): use KMS package from upstream"
    This reverts commit ce395032d3a17672df05a71748545124c7a9cabc.

--- bridge/src/activate-account.ts
+++ bridge/src/activate-account.ts
@@ -1,4 +1,4 @@
-import { KmsSigner } from "aws-kms-signer";
+import { KmsSigner } from "@planetarium/aws-kms-provider";
 import { Configuration } from "./configuration";
 import { HeadlessGraphQLClient } from "./headless-graphql-client";
 import { KMSNCGSigner } from "./kms-ncg-signer";
@@ -7,12 +7,9 @@ import crypto from "crypto";
 const ACTION = "ACTIVATE_ACCOUNT_PLAIN_VALUE";
 const headlessGraphQLClient = new HeadlessGraphQLClient(Configuration.get("GRAPHQL_API_ENDPOINT"), 5);
 
-const kmsSigner = new KmsSigner(Configuration.get("KMS_PROVIDER_KEY_ID"), {
-    region: Configuration.get("KMS_PROVIDER_REGION"),
-    credentials: {
-        accessKeyId: Configuration.get("KMS_PROVIDER_AWS_ACCESSKEY"),
-        secretAccessKey: Configuration.get("KMS_PROVIDER_AWS_SECRETKEY"),
-    },
+const kmsSigner = new KmsSigner(Configuration.get("KMS_PROVIDER_REGION"), Configuration.get("KMS_PROVIDER_KEY_ID"), {
+    accessKeyId: Configuration.get("KMS_PROVIDER_AWS_ACCESSKEY"),
+    secretAccessKey: Configuration.get("KMS_PROVIDER_AWS_SECRETKEY"),
 });
 
 const kmsNcgSigner = new KMSNCGSigner(Configuration.get("KMS_PROVIDER_REGION"), Configuration.get("KMS_PROVIDER_KEY_ID"), {
