6fadc3dfe87e0b3335e59f8532ff957fac23d365
shkangr <rkdtmdgjs@naver.com>
2023-08-01 15:02:37 +0900

    feat: added test case of whitelist wrapped event

--- bridge/test/messages/wrapped-event.spec.ts
+++ bridge/test/messages/wrapped-event.spec.ts
@@ -71,5 +71,38 @@ describe("WrappedEvent", () => {
                 ).render()
             ).toMatchSnapshot();
         });
+
+        it("snapshot with whitelist transfer", () => {
+            const EXPLORER_URL = "https://explorer.libplanet.io/9c-internal";
+            const NCSCAN_URL = "https://9cscan.com";
+            const USE_NCSCAN_URL = false;
+            const ETHERSCAN_URL = "https://ropsten.etherscan.io";
+            const SENDER = "0xCbfC996ad185c61a031f40CeeE80a055e6D83005";
+            const RECIPIENT = "0xDac65eCE9CB3E7a538773e08DE31F973233F064f";
+            const AMOUNT = "200";
+            const ETHEREUM_TRANSACTION_HASH =
+                "0x9360cd40682a91a71f0afbfac3dd381866cdb319dc01c13531dfe648f8a28bc8";
+            const NINE_CHRONICLES_TX_ID =
+                "3409cdbaa24ec6f7c8d2c0f636325a2b2e9611e5e6df5c593cfcd299860d8044";
+            const FEE = new Decimal(1);
+            const isWhitelistEvent = true;
+            expect(
+                new WrappedEvent(
+                    EXPLORER_URL,
+                    NCSCAN_URL,
+                    USE_NCSCAN_URL,
+                    ETHERSCAN_URL,
+                    SENDER,
+                    RECIPIENT,
+                    AMOUNT,
+                    NINE_CHRONICLES_TX_ID,
+                    ETHEREUM_TRANSACTION_HASH,
+                    FEE,
+                    null,
+                    null,
+                    isWhitelistEvent
+                ).render()
+            ).toMatchSnapshot();
+        });
     });
 });
