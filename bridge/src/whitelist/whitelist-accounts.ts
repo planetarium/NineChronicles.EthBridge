ccda75e694867d1c6792e35ce8c60177cd615376
shkangr <rkdtmdgjs@naver.com>
2023-08-07 10:58:39 +0900

    feat: added whitelist accounts

--- bridge/src/whitelist/whitelist-accounts.ts
+++ bridge/src/whitelist/whitelist-accounts.ts
@@ -17,4 +17,17 @@ import { ACCOUNT_TYPE } from "./account-type";
  *         description: "Account of Partner"
  *     },
  */
-export const whitelistAccounts: WhitelistAccount[] = [];
+export const whitelistAccounts: WhitelistAccount[] = [
+    {
+        type: ACCOUNT_TYPE.FEE_WAIVER_ALLOWED,
+        from: "0x7cA620bAc4b96dA636BD4Cb2141A42b55C5f6Fdd",
+        to: "0x3C3729cd7D4Ce89C6546636c8083820fac38B368",
+        description: "Nine Corporation",
+    },
+    {
+        type: ACCOUNT_TYPE.FEE_WAIVER_ALLOWED,
+        from: "0xdF81374a4e4853340CCef6485083Cc1ba9100E2B",
+        to: "0x96B32355C206d21E82cC868dd00B1C13fABdd831",
+        description: "Planetarium Labs",
+    },
+];
