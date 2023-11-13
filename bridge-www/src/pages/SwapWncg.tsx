e45f21a9a098b6e1e8e616ebdd1b9f8890c0b528
shkangr <rkdtmdgjs@naver.com>
2023-11-13 16:15:57 +0900

    feat: update bridge-www to call Contract Burn with planet-id

--- bridge-www/src/pages/SwapWncg.tsx
+++ bridge-www/src/pages/SwapWncg.tsx
@@ -3,13 +3,19 @@ import Decimal from "decimal.js";
 import { BigNumber } from "ethers";
 import { isAddress } from "ethers/lib/utils";
 import { useState, useMemo } from "react";
-import { erc20ABI, useBalance, useContract, useNetwork, useSigner } from "wagmi";
+import {
+  erc20ABI,
+  useBalance,
+  useContract,
+  useNetwork,
+  useSigner,
+} from "wagmi";
 import { TextField } from "../components/TextField";
-import { mainnet, ropsten, goerli } from 'wagmi/chains'
+import { mainnet, ropsten, goerli } from "wagmi/chains";
 
 interface SwapWncgPageProps {
-    address: string,
-};
+  address: string;
+}
 
 const SwapWncgPage: React.FC<SwapWncgPageProps> = ({ address }) => {
   const { chain } = useNetwork();
@@ -20,7 +26,8 @@ const SwapWncgPage: React.FC<SwapWncgPageProps> = ({ address }) => {
     } else if (chain?.id === ropsten.id) {
       return "0xeaa982f3424338598738c496153e55b1df11f625";
     } else if (chain?.id === goerli.id) {
-      return "0xafbf2bee0c4803c13436db44509267fa2312ad33";      
+      return "0x847933319010F86aa8294904da3A295bdAb6D540";
+      // return "0xe74aa16cb9F713F58D7c86f0e413712D4220Bc05";
     } else {
       return "0xad84431776e966400a8aecaf7bd2316a69c54efe";
     }
@@ -35,8 +42,8 @@ const SwapWncgPage: React.FC<SwapWncgPageProps> = ({ address }) => {
   const contract = useContract({
     addressOrName: contractAddress,
     contractInterface: [
-        "function burn(uint256 amount, bytes32 to) public",
-        ...erc20ABI,
+      "function burn(uint256 amount, bytes32 to) public",
+      ...erc20ABI,
     ],
     signerOrProvider: signer,
   });
@@ -45,52 +52,81 @@ const SwapWncgPage: React.FC<SwapWncgPageProps> = ({ address }) => {
   const [amount, setAmount] = useState<string>("0");
   const burnAmount = useMemo<Decimal | null>(() => {
     try {
-      return new Decimal(amount || "0").mul(new Decimal(10).pow(18))
+      return new Decimal(amount || "0").mul(new Decimal(10).pow(18));
     } catch {
       return null;
     }
   }, [amount]);
 
   return (
-    <Container style={{
-      margin: "30vh 0",
-      minWidth: "100%",
-      overflow: "hidden",
-    }}>
+    <Container
+      style={{
+        margin: "30vh 0",
+        minWidth: "100%",
+        overflow: "hidden",
+      }}
+    >
       <Row justify="center">
-        <Text>Your wNCG : {
-          isLoading
-            ? <Text span={true}>🕑</Text>
-            : <Text weight="bold" span={true}>{
-                  data !== undefined
-                      ? new Decimal(data.value.toString()).div(new Decimal(10).pow(data.decimals)).toString()
-                      : error?.message
-              }</Text>
-        }</Text>
+        <Text>
+          Your wNCG :{" "}
+          {isLoading ? (
+            <Text span={true}>🕑</Text>
+          ) : (
+            <Text weight="bold" span={true}>
+              {data !== undefined
+                ? new Decimal(data.value.toString())
+                    .div(new Decimal(10).pow(data.decimals))
+                    .toString()
+                : error?.message}
+            </Text>
+          )}
+        </Text>
       </Row>
       <Spacer />
       <Spacer />
       <Row justify="center">
-        <TextField label={'To'} onChange={setNcAddress}/>
+        <TextField label={"To"} onChange={setNcAddress} />
       </Row>
       <Spacer />
       <Row justify="center">
-        <TextField label={'Amount'} onChange={setAmount}/>
+        <TextField label={"Amount"} onChange={setAmount} />
       </Row>
       <Spacer />
       <Row justify="center">
-        {
-          contract === null || burnAmount === null || burnAmount.toString().indexOf(".") !== -1 || !isAddress(ncAddress)
-            ? <Text weight={"bold"}>Fill corret values</Text>
-            : <Button onClick={event => {
-              event.preventDefault();    
+        {contract === null ||
+        burnAmount === null ||
+        burnAmount.toString().indexOf(".") !== -1 ||
+        !isAddress(ncAddress) ? (
+          <Text weight={"bold"}>Fill corret values</Text>
+        ) : (
+          <Button
+            onClick={(event) => {
+              if (event.currentTarget === undefined) return;
+              event.preventDefault();
               console.log(contract);
-              contract.burn(BigNumber.from(burnAmount.toString()), ncAddress + "0".repeat(24)).then(console.debug)
-            }}>Burn</Button>
-        }
+
+              const planetId = "0x100000000001";
+              const _to = (planetId + ncAddress.slice(2)).padEnd(66, "0");
+
+              contract
+                .burn(BigNumber.from(burnAmount.toString()), _to)
+                .then(console.debug);
+
+              // Before Applied Mult-Planet
+              // contract
+              //   .burn(
+              //     BigNumber.from(burnAmount.toString()),
+              //     ncAddress + "0".repeat(24)
+              //   )
+              //   .then(console.debug);
+            }}
+          >
+            Burn
+          </Button>
+        )}
       </Row>
     </Container>
   );
-}
+};
 
 export default SwapWncgPage;
