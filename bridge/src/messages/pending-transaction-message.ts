0ad1e8fc1c7e5fe587b1de66e1390a06276f4389
carolk-dev <carol@planetariumhq.com>
2024-12-19 17:45:14 +0900

    fix bug

--- bridge/src/messages/pending-transaction-message.ts
+++ bridge/src/messages/pending-transaction-message.ts
@@ -6,15 +6,19 @@ import { combineUrl } from "./utils";
 import { MultiPlanetary } from "../multi-planetary";
 
 export class PendingTransactionMessage implements Message {
-    private readonly _bscScanUrl: string;
+    private readonly _ethScanUrl: string;
+    private readonly _ncScanUrl: string;
     private readonly _multiPlanetary: MultiPlanetary;
 
     constructor(
         private readonly transactions: ExchangeHistory[],
         private readonly multiPlanetary: MultiPlanetary,
-        bscScanUrl: string = process.env.BSCSCAN_URL || "https://bscscan.com"
+        ethScanUrl: string = process.env.ETHERSCAN_ROOT_URL ||
+            "https://etherscan.io/",
+        ncScanUrl: string = process.env.NCSCAN_URL || "https://9cscan.com/"
     ) {
-        this._bscScanUrl = bscScanUrl;
+        this._ethScanUrl = ethScanUrl;
+        this._ncScanUrl = ncScanUrl;
         this._multiPlanetary = multiPlanetary;
     }
 
@@ -23,41 +27,50 @@ export class PendingTransactionMessage implements Message {
             console.log("Pending Transactions : ", this.transactions);
             return {
                 text: `${this.transactions.length} Pending Transactions Found`,
-                attachments: this.transactions.map((tx) => ({
-                    author_name: "[ETH] wNCG → NCG pending event",
-                    color: "#ff0033",
-                    fields: [
-                        {
-                            title: "BSC transaction",
-                            value: combineUrl(
-                                this._bscScanUrl,
-                                `/tx/${tx.tx_id}`
-                            ),
-                        },
-                        {
-                            title: "Planet Name",
-                            value: this._multiPlanetary.getRequestPlanetName(
-                                tx.recipient
-                            ),
-                        },
-                        {
-                            title: "Sender(BSC)",
-                            value: tx.sender,
-                        },
-                        {
-                            title: "Recipient(9c)",
-                            value: tx.recipient,
-                        },
-                        {
-                            title: "Amount",
-                            value: tx.amount.toString(),
-                        },
-                        {
-                            title: "Timestamp",
-                            value: tx.timestamp,
-                        },
-                    ],
-                })),
+                attachments: this.transactions.map((tx) => {
+                    const titleUrl =
+                        tx.network === "ethereum"
+                            ? this._ethScanUrl
+                            : this._ncScanUrl;
+                    const senderLabel =
+                        tx.network === "ethereum" ? "ETH" : "9C";
+                    const recipientLabel =
+                        tx.network === "ethereum" ? "9c" : "ETH";
+                    const authorLabel =
+                        tx.network === "ethereum" ? "wNCG → NCG" : "NCG → wNCG";
+                    return {
+                        author_name: `[ETH] ${authorLabel} pending event`,
+                        color: "#ff0033",
+                        fields: [
+                            {
+                                title: senderLabel + " transaction",
+                                value: combineUrl(titleUrl, `/tx/${tx.tx_id}`),
+                            },
+                            {
+                                title: "Planet Name",
+                                value: this._multiPlanetary.getRequestPlanetName(
+                                    tx.recipient
+                                ),
+                            },
+                            {
+                                title: `Sender(${senderLabel})`,
+                                value: tx.sender,
+                            },
+                            {
+                                title: `Recipient(${recipientLabel})`,
+                                value: tx.recipient,
+                            },
+                            {
+                                title: "Amount",
+                                value: tx.amount.toString(),
+                            },
+                            {
+                                title: "Timestamp",
+                                value: tx.timestamp,
+                            },
+                        ],
+                    };
+                }),
             };
         }
 
@@ -65,7 +78,7 @@ export class PendingTransactionMessage implements Message {
             text: "No pending transactions",
             attachments: [
                 {
-                    author_name: "BSC Bridge Restarted",
+                    author_name: "ETH Bridge Restarted",
                     color: "#ffcc00",
                 },
             ],
